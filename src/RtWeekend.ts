10ba55846883a2a3099d0d76a6d724e78703491d
henripc <henriquechimetta@outlook.com>
2022-07-03 14:16:11 -0300

    Update RtWeekend class.

--- src/RtWeekend.ts
+++ src/RtWeekend.ts
@@ -5,4 +5,20 @@ export const PI = Math.PI;
 // Utility Functions
 export const degreeToRadians = (degrees: number): number => {
     return degrees * PI / 180;
-}
+};
+
+export const randomDouble = (min?: number, max?: number): number => {
+    // Returns a random real in [min,max) or [0, 1).
+    if (min && max) {
+        return min + (max - min) * randomDouble();
+    }
+
+    return Math.random();
+};
+
+export const clamp = (x: number, min: number, max: number): number => {
+    if (x < min) return min;
+    if (x > max) return max;
+            
+    return x;
+};
