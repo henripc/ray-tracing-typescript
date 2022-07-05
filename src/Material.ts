6514bef6e89adb3629d811ec32843d8111fc3c63
henripc <henriquechimetta@outlook.com>
2022-07-05 09:53:56 -0300

    Added the Dielectric material.

--- src/Material.ts
+++ src/Material.ts
@@ -2,6 +2,7 @@ import { Ray } from "./Ray";
 import { HitRecord } from "./Hittable";
 import { Color } from "./Color";
 import { Vector } from "./Vector";
+import { randomDouble } from "./RtWeekend";
 
 export interface Material {
     scatter(rIn: Ray, rec: HitRecord, attenuation: Color, scattered: Ray): boolean;
@@ -14,7 +15,7 @@ export class Lambertian implements Material {
         this.albedo = albedo;
     }
 
-    scatter(rIn: Ray, rec: HitRecord, attenuation: Color, scattered: Ray): boolean {
+    public scatter(rIn: Ray, rec: HitRecord, attenuation: Color, scattered: Ray): boolean {
         let scatteredDirection = Vector.sumOfVectors(rec.normal, Vector.randomUnitVector());
 
         // Catch degenerate scatter direction
@@ -41,7 +42,7 @@ export class Metal implements Material {
         this.fuzz = fuzz < 1 ? fuzz : 1;
     }
 
-    scatter(rIn: Ray, rec: HitRecord, attenuation: Color, scattered: Ray): boolean {
+    public scatter(rIn: Ray, rec: HitRecord, attenuation: Color, scattered: Ray): boolean {
         const reflected = Vector.reflect(Vector.unitVector(rIn.direction), rec.normal);
         scattered.orig = rec.p;
         scattered.dir = Vector.sumOfVectors(
@@ -54,4 +55,47 @@ export class Metal implements Material {
 
         return Vector.dot(scattered.direction, rec.normal) > 0;
     }
+}
+
+export class Dielectric implements Material {
+    public ir: number;  // Index of Refraction
+
+    public constructor(indexOfRefraction: number) {
+        this.ir = indexOfRefraction;
+    }
+
+    private static reflectance(cosine: number, refIdx: number): number {
+        // Use Schlick's approximation for reflectance.
+        let r0 = (1 - refIdx) / (1 + refIdx);
+        r0 = r0 * r0;
+
+        return r0 + (1 - r0) * Math.pow(1 - cosine, 5);
+    }
+
+    public scatter(rIn: Ray, rec: HitRecord, attenuation: Color, scattered: Ray): boolean {
+        attenuation.e[0] = 1;
+        attenuation.e[1] = 1;
+        attenuation.e[2] = 1;
+
+        const refractionRatio = rec.frontFace ? (1 / this.ir) : this.ir;
+
+        const unitDirection = Vector.unitVector(rIn.direction);
+        const cosTheta = Math.min(Vector.dot(unitDirection.scalarMultiplication(-1), rec.normal), 1); 
+        const sinTheta = Math.sqrt(1 - cosTheta * cosTheta);
+
+        const cannotRefract = refractionRatio * sinTheta > 1;
+        let direction: Vector;
+
+        if (cannotRefract || Dielectric.reflectance(cosTheta, refractionRatio) > randomDouble()) {
+            direction = Vector.reflect(unitDirection, rec.normal);
+        } else {
+            direction = Vector.refract(unitDirection, rec.normal, refractionRatio);
+        }
+
+        scattered.orig = rec.p;
+        scattered.dir = direction;
+
+        return true;
+    }
+
 }
