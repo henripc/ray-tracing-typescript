a9be5390ff698c7f347540f3a4f9a153f5592cce
henripc <henriquechimetta@outlook.com>
2022-07-04 22:25:21 -0300

    Added Material support for HitRecord.

--- src/Hittable.ts
+++ src/Hittable.ts
@@ -1,15 +1,19 @@
 import { Vector } from "./Vector";
 import { Ray } from "./Ray";
+import { Lambertian, Material } from "./Material";
+import { Color } from "./Color";
 
 export class HitRecord {
     public p: Vector;
     public normal: Vector;
+    public mat: Material;
     public t: number;
     public frontFace: boolean;
 
     public constructor() {
         this.p = new Vector();
         this.normal = new Vector();
+        this.mat = new Lambertian(new Color());
         this.t = 0;
         this.frontFace = false;
     }
