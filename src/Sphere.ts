1250670f7bb63cf24adc7e85a386efac117ddc97
henripc <henriquechimetta@outlook.com>
2022-07-04 22:26:18 -0300

    Added material support.

--- src/Sphere.ts
+++ src/Sphere.ts
@@ -1,14 +1,17 @@
 import { HitRecord, Hittable } from "./Hittable";
+import { Material } from "./Material";
 import { Ray } from "./Ray";
 import { Vector } from "./Vector";
 
 export class Sphere implements Hittable {
     public center: Vector;
     public radius: number;
+    public material: Material;
 
-    public constructor(center: Vector, radius: number) {
+    public constructor(center: Vector, radius: number, material: Material) {
         this.center = center;
         this.radius = radius;
+        this.material = material;
     }
 
     public hit(r: Ray, tMin: number, tMax: number, rec: HitRecord): boolean {
@@ -35,6 +38,7 @@ export class Sphere implements Hittable {
         rec.p = r.at(rec.t);
         const outwardNormal = Vector.sumOfVectors(rec.p, this.center.scalarMultiplication(-1)).scalarDivision(this.radius);
         rec.setFaceNormal(r, outwardNormal);
+        rec.mat = this.material;
 
         return true;
     }
